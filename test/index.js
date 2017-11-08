8471ef636d9c46b2011c9f981435715e716aab11
Jon Ribeiro <contact@jonathas.com>
2017-11-09 00:07:44 +0100

    Refactor the lib to async/await and refactor the tests

--- test/index.js
+++ test/index.js
@@ -1,100 +1,70 @@
-"use strict";
+let fs = require('fs');
+let path = require('path');
+require('chai').expect();
+require('chai').should();
+let pdf2img = require('../index.js');
 
-var fs = require('fs');
-var path = require('path');
-var expect = require('chai').expect();
-var should = require('chai').should();
-var pdf2img = require('../index.js');
-
-var input = __dirname + path.sep + 'test.pdf';
+let input = `${__dirname}${path.sep}test.pdf`;
 
 pdf2img.setOptions({
-    outputdir: __dirname + path.sep + '/output',
+    outputdir: `${__dirname}${path.sep}output`,
     outputname: 'test'
 });
 
-describe('Split and convert pdf into images', () => {
+const checkMessages = (info, fileType = 'jpg') => {
+    let n = 1;
+    for (let file of info.message) {
+        file.page.should.equal(n);
+        file.name.should.equal(`test_${n}.${fileType}`);
+        fs.statSync(file.path).isFile().should.be.true;
+        if (n === 3) return true;
+        n++;
+    }
+};
 
-    it.only('Create jpg files', function(done) {
-        this.timeout(100000);
-        pdf2img.convert(input, (err, info) => {
-            if (info.result !== 'success') {
+const checkSingleMessage = (info, page, fileType = 'jpg') => {
+    info.message.length.should.equal(1);
+    let file = info.message[0];
+    file.page.should.equal(page);
+    file.name.should.equal(`test_${page}.${fileType}`);
+    fs.statSync(file.path).isFile().should.be.true;
+};
+
+describe('# Pdf2Img', function() {
+    this.timeout(100000);
+
+    it('should create jpg files', function() {
+        return pdf2img.convert(input)
+            .then(info => {
                 info.result.should.equal('success');
-                done();
-            } else {
-                var n = 1;
-                info.message.forEach(function(file) {
-                    file.page.should.equal(n);
-                    file.name.should.equal('test_' + n + '.jpg');
-                    isFileExists(file.path).should.to.be.true;
-                    if (n === 3) done();
-                    n++;
-                });
-            }
-        });
+                checkMessages(info);
+            });
     });
 
-    it('Create png files', function(done) {
-        this.timeout(100000);
+    it('should create png files', function() {
         pdf2img.setOptions({ type: 'png' });
-        pdf2img.convert(input, function(err, info) {
-            if (info.result !== 'success') {
+        return pdf2img.convert(input)
+            .then(info => {
                 info.result.should.equal('success');
-                done();
-            } else {
-                var n = 1;
-                info.message.forEach(function(file) {
-                    file.page.should.equal(n);
-                    file.name.should.equal('test_' + n + '.png');
-                    isFileExists(file.path).should.to.be.true;
-                    if (n === 3) done();
-                    n++;
-                });
-            }
-        });
+                checkMessages(info, 'png');
+            });
     });
 
-    it('Create jpg file only for given page', function(done) {
-        this.timeout(100000);
+    it('should create jpg file only for given page', () => {
         pdf2img.setOptions({ type: 'jpg', page: 1 });
-        pdf2img.convert(input, function(err, info) {
-            if (info.result !== 'success') {
+        return pdf2img.convert(input)
+            .then(info => {
                 info.result.should.equal('success');
-                done();
-            } else {
-                info.message.length.should.equal(1)
-                var file = info.message[0];
-                file.page.should.equal(1);
-                file.name.should.equal('test_1.jpg');
-                isFileExists(file.path).should.to.be.true;
-                done();
-            }
-        });
+                checkSingleMessage(info, 1);
+            });
     });
 
-    it('Create png file only for given page', function(done) {
-        this.timeout(100000);
+    it('should create png file only for given page', () => {
         pdf2img.setOptions({ type: 'png', page: 2 });
-        pdf2img.convert(input, function(err, info) {
-            if (info.result !== 'success') {
+        return pdf2img.convert(input)
+            .then(info => {
                 info.result.should.equal('success');
-                done();
-            } else {
-                info.message.length.should.equal(1)
-                var file = info.message[0];
-                file.page.should.equal(2);
-                file.name.should.equal('test_2.png');
-                isFileExists(file.path).should.to.be.true;
-                done();
-            }
-        });
+                checkSingleMessage(info, 2, 'png');
+            });
     });
 });
-
-var isFileExists = function(path) {
-    try {
-        return fs.statSync(path).isFile();
-    } catch (e) {
-        return false;
-    }
-}
