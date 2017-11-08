0ddcd376284b7b5517d3886cd1118ca79c3bade5
Jon Ribeiro <contact@jonathas.com>
2017-11-08 20:26:17 +0100

    Remove async lib dependency

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 "use strict";
 
-module.exports = require('./lib/pdf2img');
+const child = require('child_process');
+const pdf2img = require('./lib/pdf2img');
+
+module.exports = (() => {
+    child.exec(`which graphicsmagick`, (err, stdout, stderr) => {
+        if (err) {
+            console.error('Please install graphicsmagick in order to run pdf2img');
+            process.exit(1);
+        }
+        return pdf2img;
+    });
+})();
