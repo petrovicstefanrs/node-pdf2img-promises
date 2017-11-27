da47fb4a4283aa6d8ad32b1972972a859b60e591
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2017-11-27 18:46:48 +0100

    Fixed tests

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ let path = require('path');
 const chai = require('chai');
 chai.expect();
 chai.should();
-let pdf2img = require('../index.js');
+const Pdf2img = require('../index.js');
 const mkdir = util.promisify(fs.mkdir);
 const rmdir = util.promisify(fs.rmdir);
 const unlink = util.promisify(fs.unlink);
@@ -17,7 +17,9 @@ const testDir = `${__dirname}${path.sep}testdir.pdf`;
 const outputDir = `${__dirname}${path.sep}output`;
 const fakeFile = `${__dirname}${path.sep}fake.pdf`;
 
-pdf2img.setOptions({
+const converter = new Pdf2img();
+
+converter.setOptions({
     outputdir: outputDir,
     outputname: 'test'
 });
@@ -41,20 +43,22 @@ const checkSingleMessage = (info, page, fileType = 'jpg') => {
     fs.statSync(file.path).isFile().should.be.true;
 };
 
-describe('# Pdf2Img', function() {
+describe('# Pdf2Img', function () {
     this.timeout(100000);
 
-    it('should create jpg files', function() {
-        return pdf2img.convert(input)
+    it('should create jpg files', function () {
+        return converter.convert(input)
             .then(info => {
                 info.result.should.equal('success');
                 checkMessages(info);
             });
     });
 
-    it('should create png files', function() {
-        pdf2img.setOptions({ type: 'png' });
-        return pdf2img.convert(input)
+    it('should create png files', function () {
+        converter.setOptions({
+            type: 'png'
+        });
+        return converter.convert(input)
             .then(info => {
                 info.result.should.equal('success');
                 checkMessages(info, 'png');
@@ -62,8 +66,11 @@ describe('# Pdf2Img', function() {
     });
 
     it('should create jpg file only for given page', () => {
-        pdf2img.setOptions({ type: 'jpg', page: 1 });
-        return pdf2img.convert(input)
+        converter.setOptions({
+            type: 'jpg',
+            page: 1
+        });
+        return converter.convert(input)
             .then(info => {
                 info.result.should.equal('success');
                 checkSingleMessage(info, 1);
@@ -71,8 +78,11 @@ describe('# Pdf2Img', function() {
     });
 
     it('should create png file only for given page', () => {
-        pdf2img.setOptions({ type: 'png', page: 2 });
-        return pdf2img.convert(input)
+        converter.setOptions({
+            type: 'png',
+            page: 2
+        });
+        return converter.convert(input)
             .then(info => {
                 info.result.should.equal('success');
                 checkSingleMessage(info, 2, 'png');
@@ -80,32 +90,27 @@ describe('# Pdf2Img', function() {
     });
 
     it('should throw an error on invalid input path', () => {
-        return pdf2img.convert('').catch(err => {
+        return converter.convert('').catch(err => {
             chai.expect(err.message).to.equal('Invalid input file path.');
         });
     });
 
     it('should throw an error on unsupported file type', () => {
-        return pdf2img.convert(`${__dirname}${path.sep}index.js`).catch(err => {
+        return converter.convert(`${__dirname}${path.sep}index.js`).catch(err => {
             chai.expect(err.message).to.equal('Unsupported file type.');
         });
     });
 
     it('should throw an error when the input file is not a file', () => {
         return mkdir(testDir)
-            .then(() => pdf2img.convert(testDir))
+            .then(() => converter.convert(testDir))
             .catch(err => chai.expect(err.message).to.equal('Input file not found.'));
     });
 
     it('should throw an error when trying to get the number of pages of a fake file', () => {
         return writeFile(fakeFile, 'Hello there, I am not really a pdf file', 'utf8')
-            .then(() => pdf2img.convert(fakeFile))
+            .then(() => converter.convert(fakeFile))
             .catch(err => chai.expect(err.message).to.contain('Command failed'));
-    });
-
-    it('should get a new instance of the pdf2img class', () => {
-        let newInstance = pdf2img.getNewInstance();
-        chai.expect(newInstance.options.outputname).to.not.equal('test');
     });
 
     after(() => {
